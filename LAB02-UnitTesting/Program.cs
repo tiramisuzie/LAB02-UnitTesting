76135ee1578442f83be789e0cc86d83c40e83b35
suzanne su <suhysu@gmail.com>
2018-10-10 01:19:33 -0700

    added conditionals to user input

--- LAB02-UnitTesting/Program.cs
+++ LAB02-UnitTesting/Program.cs
@@ -4,30 +4,46 @@ namespace LAB02_UnitTesting
 {
     class Program
     {
+        public static double balance = 1000;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to PseudoBank");
         }
 
-        static void UserInput()
+        public static void UserInput()
         {
             Console.WriteLine("Please select from the following options.");
             Console.WriteLine("Press 1 to view balance");
             Console.WriteLine("Press 2 to withdraw money");
             Console.WriteLine("Press 3 to add money");
+
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine(ViewBalance());
+            }
+            else if (input == "2")
+            {
+                WithdrawMoney();
+            }
+            else if (input == "3")
+            {
+                AddMoney();
+            }
         }
 
-        static void ViewBalance()
+        public static double ViewBalance()
         {
-
+            return balance;
         }
 
-        static void WithdrawMoney()
+        public static void WithdrawMoney()
         {
-
+            Console.ReadLine();
         }
 
-        static void AddMoney()
+        public static void AddMoney()
         {
 
         }
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LAB02_UnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
